46be1b74f514bdd30ca80dee31d2f56cedb9119d
Jose Arguello <johao5991@gmail.com>
2019-09-16 12:57:05 -0400

    MongoDB Mongoose with Mocha

---
+++ test/saving_test.js
@@ -0,0 +1,10 @@
+const mocha = require("mocha");
+const assert = require("assert");
+
+// Describe Tests *Put here what your test is about*
+describe("some demo tests", () => {
+  // Create Tests
+  it("adds Two Numbers together", () => {
+    assert(2 + 3 === 5);
+  });
+});
